814fdec83ac1c91e93b7f558778edfe20c78dc78
Val_Brin <59572430+valbrin@users.noreply.github.com>
2021-12-11 17:11:54 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,22 @@
+<script>
 $(document).ready(function () {
+    // $('.t-submit').on('click', function () {
+    //     var attr = $('.t-img-select__hiddeninput js-tilda-rule').attr('value');
+    //     // var http = 'https://static.tildacdn.info/tild6134-3966-4266-b339-373762336336/8c201547e3b3089846f2.jpg';
 
-    $('.answer').click(function(){
-        
-        $('button').attr()
-        // alert('button');
-    });
+    //     $('.t-img-select__container').hide().alert(attr);
+    // });
+    
+    $('button.t-submit').click(function(){
+        var $attr = $('.t-input-block input.t-img-select__hiddeninput').attr('value');
+        var $answr1 = 'https://static.tildacdn.info/tild3930-3136-4932-b662-336261306132/3e9e2c647b2ac86b632e.jpg';
+        var $answr2 = 'https://static.tildacdn.info/tild6235-3562-4762-b561-363530366661/3e9e2c647b2ac86b632e.jpg';
 
-    $('.quiz_btn').on('click', function () {
-        if ($())
-        // $('.answer').hide().text('Real sou real').show(alert('Realnostichno slishkom. poprobovat snova?'));
+        if($attr == $answr1 || $answr2){
+            return alert('OK');
+        }else{
+            return alert('false');
+        };
     });
 });
+</script>
